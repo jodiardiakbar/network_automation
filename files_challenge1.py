ff6e0932bfd0c0e8d05cfa476b1b69ebfcfb61d2
Jodiardi Akbar <jodiardi.akbar@gmail.com>
2024-01-13 11:58:32 -0500

    Update files_challenge1.py

--- files_challenge1.py
+++ files_challenge1.py
@@ -1,5 +1,18 @@
 #!./venv/bin/python
 
+#####   #####   #####
+# Author        : Jodiardi Akbar
+# Date Created  : 5 Jan 2024
+# Date Modified : -
+# Description   : duplicate mac address remover
+# Usage         : files_challenge1.py
+#####   #####   #####
+
+"""
+Consider this text file that contains multiple duplicate MAC addresses.
+Create a new file that contains only unique MAC addresses. Each MAC should be on its own line.
+"""
+
 if __name__ == '__main__':
     with open('macs.txt', 'r') as f:
         all_macs = f.read().split()
