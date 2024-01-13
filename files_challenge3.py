3fb01bf21c726873fc530a8d367adacf035df088
Jodiardi Akbar <jodiardi.akbar@gmail.com>
2024-01-13 12:01:55 -0500

    Update files_challenge3.py

--- files_challenge3.py
+++ files_challenge3.py
@@ -1,5 +1,18 @@
 #!./venv/bin/python
 
+#####   #####   #####
+# Author        : Jodiardi Akbar
+# Date Created  : 5 Jan 2024
+# Date Modified : -
+# Description   : remove all empty lines including spaces
+# Usage         : files_challenge3.py
+#####   #####   #####
+
+"""
+Create a Python script that removes all empty lines 
+including those that contain only spaces from a file.
+"""
+
 if __name__ == '__main__':
     with open('file.txt', 'r') as f:
         content = f.read().split()
