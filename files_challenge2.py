ffb0e5666ac736de5d688ba09127bfbc6d444703
Jodiardi Akbar <jodiardi.akbar@gmail.com>
2024-01-13 12:00:24 -0500

    Update files_challenge2.py

--- files_challenge2.py
+++ files_challenge2.py
@@ -1,5 +1,18 @@
 #!./venv/bin/python
 
+#####   #####   #####
+# Author        : Jodiardi Akbar
+# Date Created  : 5 Jan 2024
+# Date Modified : -
+# Description   : from file to a list to a string and print
+# Usage         : files_challenge2.py
+#####   #####   #####
+
+"""
+Create a Python script that reads a text file into a list 
+and then converts the list into a string that has the entire file content.
+"""
+
 if __name__ == '__main__':
     with open('sample_file.txt', 'r') as f:
         content_as_list = f.read().split()
